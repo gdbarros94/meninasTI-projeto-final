336927af9a2892f8e6234bcb5ce54ed56027d436
Gabriel Pires de Barros <gdbarros95@gmail.com>
2023-12-04 17:08:32 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,28 @@
+var livros = [
+    {titulo:,
+    ano:
+autor:
+imgCapa:
+}
+]
+
+var produtos = [
+    {
+        nome:
+        desc:
+        valor:
+        peso:
+        imagemCapa:
+        galeria: ["imagem1.jpg", "imagem2.jpg"...],
+        categoria: "calca",
+        tag: "jeans,cintura alta, ...",
+        
+    }
+]
+
+var configuracaoSite = [
+    {logo: "logo.png",
+    titulo: "sdfdfsd",
+    ...    
+    }
+]
